bd8bc89cea68c50d4482c5004386a1134694b448
Evdokimov11 <Evdokimov11.1996@gmail.com>
2023-03-04 02:21:57 +0400

    Make code for pep8 standart

--- main.py
+++ main.py
@@ -6,20 +6,19 @@ from dotenv import load_dotenv
 
 VK_API_VERSION = 5.131
 
-def check_status(formatted_response):
-  
-    try :
-      error_msg = formatted_response['error']['error_msg']
-      error_code = formatted_response['error']['error_code']
-      full_error_msg = f'Code error : {error_code}. {error_msg}'
 
-      return True, full_error_msg
-      
-      exit(full_error_msg)
-      
+def check_status(formatted_response):
+
+    try:
+        error_msg = formatted_response['error']['error_msg']
+        error_code = formatted_response['error']['error_code']
+        full_error_msg = f'Code error : {error_code}. {error_msg}'
+
+        return True, full_error_msg
+
     except KeyError:
-      
-      return False, 'Ok'
+
+        return False, 'Ok'
 
 
 def get_rand_comics():
@@ -54,20 +53,20 @@ def get_upload_url(vk_headers, vk_group_id):
 
     request_url = 'https://api.vk.com/method/photos.getWallUploadServer'
     params = {
-      'group_id': vk_group_id,
-      'v': VK_API_VERSION,
+        'group_id': vk_group_id,
+        'v': VK_API_VERSION,
     }
-    
+
     response = requests.post(request_url, params=params, headers=vk_headers)
-    response.raise_for_status()     
+    response.raise_for_status()
     formatted_response = response.json()
-    
+
     error_key, error_message = check_status(formatted_response)
-    if error_key : 
-      exit(error_message)
-    
+    if error_key:
+        exit(error_message)
+
     upload_photo_url = formatted_response['response']['upload_url']
-       
+
     return upload_photo_url
 
 
@@ -77,47 +76,47 @@ def upload_photo(vk_headers, upload_photo_url):
 
         files = {'photo': file}
         upload_photo_response = requests.post(
-          upload_photo_url,
-          files=files,
-          headers=vk_headers,
+            upload_photo_url,
+            files=files,
+            headers=vk_headers,
         )
 
     upload_photo_response.raise_for_status()
     upload_photo_formatted_response = upload_photo_response.json()
     error_key, error_message = check_status(upload_photo_formatted_response)
-    if error_key : 
-      exit(error_message)
-        
+    if error_key:
+        exit(error_message)
+
     return upload_photo_formatted_response
 
 
-def save_photo(vk_headers, upload_photo, upload_hash, 
-               upload_server, vk_group_id):
+def save_photo(vk_headers, upload_photo, upload_hash, upload_server,
+               vk_group_id):
 
     request_url = 'https://api.vk.com/method/photos.saveWallPhoto'
 
     vk_params = {
-      'group_id': vk_group_id,
-      'v': VK_API_VERSION,
-      'photo': upload_photo,
-      'hash': upload_hash,
-      'server': upload_server,
+        'group_id': vk_group_id,
+        'v': VK_API_VERSION,
+        'photo': upload_photo,
+        'hash': upload_hash,
+        'server': upload_server,
     }
 
     vk_response = requests.post(
-      request_url,
-      params=vk_params,
-      headers=vk_headers,
+        request_url,
+        params=vk_params,
+        headers=vk_headers,
     )
     vk_response.raise_for_status()
     formatted_vk_response = vk_response.json()
-    
+
     error_key, error_message = check_status(formatted_vk_response)
-    if error_key : 
-      exit(error_message)
+    if error_key:
+        exit(error_message)
 
     person_id = formatted_vk_response['response'][0]['owner_id']
-    photo_id =  formatted_vk_response['response'][0]['id']
+    photo_id = formatted_vk_response['response'][0]['id']
 
     return person_id, photo_id
 
@@ -132,13 +131,13 @@ def publish_photo(vk_headers, person_id, photo_id, vk_group_id, comics_coment):
         'message': comics_coment,
         'attachments': attachments,
         'v': VK_API_VERSION,
-        }
+    }
 
     publish_response = requests.post(
         publish_url,
         params=publish_params,
         headers=vk_headers,
-        )
+    )
     publish_response.raise_for_status()
 
 
@@ -153,7 +152,7 @@ if __name__ == '__main__':
 
     comics_comment = get_rand_comics()
     try:
-      
+
         upload_photo_url = get_upload_url(vk_headers, vk_group_id)
         upload_photo_response = upload_photo(vk_headers, upload_photo_url)
         upload_photo = upload_photo_response['photo'],
@@ -165,9 +164,15 @@ if __name__ == '__main__':
             upload_hash,
             upload_server,
             vk_group_id,
-            )
-        publish_photo(vk_headers, person_id, photo_id, vk_group_id, comics_comment)
-      
+        )
+        publish_photo(
+            vk_headers,
+            person_id,
+            photo_id,
+            vk_group_id,
+            comics_comment,
+        )
+
     finally:
-      
+
         os.remove('comics.png')
