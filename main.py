3e84313be077de08bf7acb6c2b945563178b543f
Evdokimov11 <Evdokimov11.1996@gmail.com>
2023-03-03 21:47:55 +0400

    Change name of variables

--- main.py
+++ main.py
@@ -9,22 +9,22 @@ VK_API_VERSION = 5.131
 
 def get_rand_comics():
 
-    url_last_comics = "https://xkcd.com/info.0.json"
+    last_comics_url = "https://xkcd.com/info.0.json"
     
-    last_comics_response = requests.get(url_last_comics)
+    last_comics_response = requests.get(last_comics_url)
     last_comics_response.raise_for_status()
-    last_comics_information = last_comics_response.json()
-    number_comics = last_comics_information['num']
+    last_comics = last_comics_response.json()
+    comics_number = last_comics['num']
     
-    rand_number_comics = random.randint(1,number_comics)
-    comics_url = f"https://xkcd.com/{rand_number_comics}/info.0.json"
+    rand_comics_number = random.randint(1,comics_number)
+    comics_url = f"https://xkcd.com/{rand_comics_number}/info.0.json"
     comics_filename = 'comics.png'
     
     comics_response = requests.get(comics_url)
     comics_response.raise_for_status()
-    comics_information = comics_response.json()
-    img_url = comics_information['img']
-    comics_comment = comics_information['alt']
+    comics = comics_response.json()
+    img_url = comics['img']
+    comics_comment = comics['alt']
     
     comics_image_response = requests.get(img_url)
     comics_image_response.raise_for_status()
@@ -37,17 +37,17 @@ def get_rand_comics():
 
 def get_upload_url(vk_headers,vk_group_id):
 
-        url_request = 'https://api.vk.com/method/photos.getWallUploadServer'
+        request_url = 'https://api.vk.com/method/photos.getWallUploadServer'
         params = {
                     'group_id': vk_group_id,
                     'v': VK_API_VERSION
                     }
   
-        response = requests.post(url_request,params=params,headers=vk_headers)
+        response = requests.post(request_url,params=params,headers=vk_headers)
         response.raise_for_status()
-        response_formatted = response.json()
+        formatted_response = response.json()
 
-        return response_formatted['response']['upload_url']
+        return formatted_response['response']['upload_url']
 
 
 def upload_photo(vk_headers, upload_photo_url):
@@ -60,28 +60,28 @@ def upload_photo(vk_headers, upload_photo_url):
       
         upload_photo_response = requests.post(upload_photo_url,files=files,headers=vk_headers)
         upload_photo_response.raise_for_status()
-        upload_photo_response_formatted = upload_photo_response.json()
+        upload_photo_formatted_response = upload_photo_response.json()
 
-    return upload_photo_response_formatted
+    return upload_photo_formatted_response
 
 
 def save_photo(vk_headers,upload_photo_response,vk_group_id):
 
-    saved_url_request = 'https://api.vk.com/method/photos.saveWallPhoto'
+    saved_request_url = 'https://api.vk.com/method/photos.saveWallPhoto'
   
-    vk_params_save = {
+    save_vk_params = {
             'group_id': vk_group_id,
             'v': VK_API_VERSION,
             'photo':upload_photo_response['photo'],
             'hash':upload_photo_response['hash'],
             'server':upload_photo_response['server']
             }
-    vk_response_save = requests.post(saved_url_request, params=vk_params_save,headers=vk_headers)
-    vk_response_save.raise_for_status()
-    vk_response_formatted_save = vk_response_save.json()
+    save_vk_response = requests.post(saved_request_url, params=save_vk_params,headers=vk_headers)
+    save_vk_response.raise_for_status()
+    save_formatted_vk_response = vk_response_save.json()
     
-    person_id = vk_response_formatted_save['response'][0]['owner_id']
-    photo_id = vk_response_formatted_save['response'][0]['id']
+    person_id = save_formatted_vk_response['response'][0]['owner_id']
+    photo_id = save_formatted_vk_response['response'][0]['id']
 
     return person_id, photo_id
 
@@ -97,8 +97,8 @@ def publish_photo(vk_headers,person_id,photo_id,vk_group_id, comics_comment):
                 'v': VK_API_VERSION
                 }
   
-    response_publish = requests.post(publish_url, params=publish_params,  headers=vk_headers)
-    response_publish.raise_for_status()
+    publish_response = requests.post(publish_url, params=publish_params,  headers=vk_headers)
+    publish_response.raise_for_status()
 
 
 if __name__ == '__main__':
